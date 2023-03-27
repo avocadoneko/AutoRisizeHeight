9aa2fa54fabf2ec95cffb67b15c38ac6ff8f10a9
NatsuhoKimura <kimnatsu0815@gmail.com>
2023-03-27 14:25:53 -0700

    first commit

---
+++ code.js
@@ -0,0 +1,30 @@
+function resizeFrameHeight(frame) {
+  let maxY = 0;
+
+  frame.children.forEach((child) => {
+    maxY = Math.max(maxY, child.y + child.height);
+  });
+
+  const newHeight = maxY;
+  frame.resize(frame.width, newHeight);
+}
+
+function main() {
+  const nodes = figma.currentPage.selection;
+
+  if (nodes.length === 0) {
+    figma.notify('No frame selected');
+    return;
+  }
+
+  nodes.forEach((node) => {
+    if (node.type === 'FRAME') {
+      resizeFrameHeight(node);
+    } else {
+      figma.notify('Selected item is not a frame');
+    }
+  });
+}
+
+main();
+figma.closePlugin();
